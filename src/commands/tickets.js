410784cc9bf7be853fc767415861a3f767ec0b2c
Neon <saahilattud@gmail.com>
2021-08-08 13:15:38 -0400

    [DEV]
    file render ../.../

--- src/commands/tickets.js
+++ src/commands/tickets.js
@@ -1,4 +1,5 @@
 let model = require('../models/tickets')
+let { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
 /**
  * @name Tickets
  */
@@ -12,6 +13,16 @@ switch(cmd) {
     case 'setCatagory':
         if(!args[0]) return message.reply("Missing Args , Channel id")
         let channel = message.guild.channels.cache.get(args[0])
+        if(!channel) return message.reply("Invalid channel")
+        if(!channel.type === 'GUILD_CATEGORY') return message.reply("Invalid")
+        if(!data) data = {}
+        data.catagory = channel.id 
+
+        client.db.set('ticket_' + message.guild.id, data).then(() => { message.reply("Done!")})
+    break;
+    case 'message':  
+    message.channel.send({ embeds: [new MessageEmbed().setTitle('Tickets').setDescription('Click the button to use tickets').setTimestamp()], components: [new MessageActionRow().addComponents(new MessageButton().setCustomId('tickets_create').setStyle('PRIMARY').setLabel(data.label || "Click for a ticket"))]})
+
     break;
 default: 
 if(!cmd) return;
