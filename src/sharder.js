6ff505905c1e8f9c111a2d46af2c67917f2bf58c
neon <saahilattud@gmail.com>
2021-08-11 13:14:16 -0400

    [DEV] c-sync

--- src/sharder.js
+++ src/sharder.js
@@ -1,5 +1,3 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager(`${__dirname}/index.js`, { totalShards: 2 });
-
-manager.spawn();
-manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
+/**
+ * @deprecated
+ */
