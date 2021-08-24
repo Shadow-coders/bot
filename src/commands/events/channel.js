8913884f2c9685a1f3da50c1b2a32760a1d22ad9
Neon <saahilattud@gmail.com>
2021-08-23 20:29:49 -0400

    Update channel.js

--- src/commands/events/channel.js
+++ src/commands/events/channel.js
@@ -46,4 +46,46 @@ break;
         let embeds = [new MessageEmbed().setTitle(`new Channel`).setDescription(desc).setColor('NOT_QUITE_BLACK').setTimestamp()]
         ch.send({ embeds })
     }
+},
+{
+    name: 'channelDelete',
+    async exeucte(channel,client) {
+        if(!channel.guild) return client.error('New dm channel created');
+        const ch = await client.db.get('chlogs_' + channel.guild.id) ? client.channels.cache.get(await client.db.get('chlogs_' + channel.guild.id)) : null
+        if(!ch) return;
+        /**
+         * @returns {String}
+         */
+        let desc; 
+        switch(channel.type) {
+case 'GUILD_TEXT':  desc = `channel deleted \nName: ${ch.name},\n ID: ${ch.id} \n NSFW ${channel.nsfw ? 'yes' : 'no'} \n Topic: ${channel.topic}`
+break;
+case 'GROUP_DM':
+client.error('group dm found')
+break;
+case 'GUILD_STAGE_VOICE':  
+desc = `Stage Channel deleted \nName: ${channel.name} \n ID: ${channel.id} \n User limit: ${channel.userLimit} \n bitrate: ${channel.bitrate} \n Topic ${channel.topic}`
+break;
+case 'GUILD_VOICE': 
+desc = `voice Channel deleted \nName: ${channel.name} \n ID: ${channel.id} \n User limit: ${channel.userLimit} \n bitrate: ${channel.bitrate} `
+break
+case 'GUILD_NEWS': desc = `channel deleted\nName: ${ch.name},\n ID: ${ch.id} \n NSFW ${channel.nsfw ? 'yes' : 'no'} \n Topic: ${channel.topic}`
+break
+case 'GUILD_CATEGORY': desc = `catagory deleted\nName: ${ch.name} \n ID: ${ch.id} \n pos ${channel.rawPosition}` 
+break;
+case 'GUILD_NEWS_THREAD': desc = `thread deleted \nName: ${ch.name} \n autoArchiveDuration: ${require('ms')(channel.autoArchiveDuration)} \n ID: ${channel.id} \n creator: <@${channel.ownerId}> (${channel.ownerId})`
+break
+case 'GUILD_PRIVATE_THREAD': desc = `thread deleted \nName: ${ch.name} \n autoArchiveDuration: ${require('ms')(channel.autoArchiveDuration)} \n ID: ${channel.id} \n creator: <@${channel.ownerId}> (${channel.ownerId})`
+break
+case 'GUILD_PUBLIC_THREAD': desc = `thread deleted \nName: ${ch.name} \n autoArchiveDuration: ${require('ms')(channel.autoArchiveDuration)} \n ID: ${channel.id} \n creator: <@${channel.ownerId}> (${channel.ownerId})`
+break
+default:
+    client.error(channel)
+break;
+        }
+       if(!desc) return;
+        let embeds = [new MessageEmbed().setTitle(`new Channel`).setDescription(desc).setColor('NOT_QUITE_BLACK').setTimestamp()]
+        ch.send({ embeds })
+    },
+    type: 'event',
 }]
