a16a8252c2ae08b431399f001771065bd21ccb08
BetaDv <62099784+BetaDv@users.noreply.github.com>
2021-09-01 22:39:42 +0300

    Update afkCheckMsg.js

--- src/commands/events/afkCheckMsg.js
+++ src/commands/events/afkCheckMsg.js
@@ -5,16 +5,18 @@ module.exports = {
     type: 'event',
     async execute(message, client) {
        if(message.author.bot) return;
-       if(client.db.get(`afk_${message.author.id}`) !== null) return;
+       if(await client.db.get(`afk_${message.author.id}`) !== null) return;
         try {
             const sukdik = new MessageEmbed()
             .setAuthor('Is Not AFK Anymore', message.author.displayAvatarURL({ dynamic : true }))
             .setDescription(`User <@${message.author.id}> just came Back from Being AFK.`)
             .setColor('#f5f50a')
-            let data = client.db.get("afk_" + message.author.id);
+            let data = await client.db.get("afk_" + message.author.id);
             message.member.setNickname(data.name);
-            return message.channel.send({ embeds: [sukdik] }); // I like that embed name very lot mao
-            client.db.delete("afk_" + message.author.id)
+            return message.channel.send({ embeds: [sukdik] }).then(msg => {
+setTimeout(() => { msg.delete() }, 5000)
+}); // I like that embed name very lot mao
+            await client.db.delete("afk_" + message.author.id)
            } catch(error) {
                client.error(error)
            }
