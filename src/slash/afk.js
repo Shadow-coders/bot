453ed50f5fcf3168f1e3f341a7ea045336e56766
BetaDv <62099784+BetaDv@users.noreply.github.com>
2021-09-02 01:17:20 +0300

    Update afk.js

--- src/slash/afk.js
+++ src/slash/afk.js
@@ -1,6 +1,19 @@
 module.exports = {
 name: 'afk',
 type: 'slash',
+data: {
+  options: [
+    SlashCommandStringOption {
+      name: 'reason',
+      description: 'Why you wana go afk',
+      required: false,
+      type: 3,
+      choices: undefined
+    }
+  ],
+  name: 'afk',
+  description: 'Go afk or something'
+},
 execute(interaction,cmd,args,client) {
 const { message, member } = interaction;
 let afkC = require("../commands/afk").execute(message, 
