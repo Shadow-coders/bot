bcec7a79c8f167940a0745f34ed3d11c9f067ea2
BetaDv <62099784+BetaDv@users.noreply.github.com>
2021-09-01 22:29:15 +0300

    Create afkCheckEdit.js

---
+++ src/commands/events/afkCheckEdit.js
@@ -0,0 +1,26 @@
+const { MessageEmbed, Message } = require('discord.js')
+module.exports = {
+    name: 'messageUpdate',
+    once: false,
+    type: 'event',
+    async execute(oldMessage, newMessage, client) {
+       if(newMessage.author.bot) return;
+       if(client.db.get(`afk_${newMessage.author.id}`) !== null) return;
+        try {
+            const sukdik = new MessageEmbed()
+            .setAuthor('Is Not AFK Anymore', newMessage.author.displayAvatarURL({ dynamic : true }))
+            .setDescription(`**User** <@${newMessage.author.id}> just came Back from Being AFK.`)
+            .setColor('#f5f50a')
+            let data = client.db.get("afk_" + message.author.id);
+            message.member.setNickname(data.name);
+            return newMessage.channel.send({ embeds: [sukdik] }).then(msg => {
+              setTimeout(()=>{
+                  msg.delete();
+              }, 5000)
+            }); // I like that embed name very lot mao
+            client.db.delete("afk_" + newMessage.author.id)
+           } catch(error) {
+               client.error(error)
+           }
+    }
+}
