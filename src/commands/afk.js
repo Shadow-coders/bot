64fea94ea4aa297c21166ea805ebb49d79481dc3
BetaDv <62099784+BetaDv@users.noreply.github.com>
2021-09-02 03:35:55 +0300

    Update afk.js

--- src/commands/afk.js
+++ src/commands/afk.js
@@ -2,17 +2,17 @@ module.exports = {
   name: "afk",
   permissions: [],
   description: "Set your Status to AFK.",
-  execute(message, args, client) {
-    if(client.db.get("afk_" + message.author.id) !== null) return;
+  async execute(message, args, client) {
+    if(await client.db.get("afk_" + message.author.id) !== null) return;
     let reason = args.join(" ") || null;
     let oldName = message.member.nickname || message.author.username;
 
-    client.db.set(`afk_${message.author.id}`, {
+    await client.db.set(`afk_${message.author.id}`, {
       reason,
       name: oldName
     })
 
     message.member.setNickname("[AFK] " + oldName);
-    return message.channel.send(`Your Status was Set to "AFK". ${(reason === null) ? "" : "Your Reasoning is: " + reason}`);
+    return message.channel.send(`Your Status was Set to "AFK". ${(reason === null) ? "" : "\nYour Reasoning is: " + reason}`);
   }
 };
