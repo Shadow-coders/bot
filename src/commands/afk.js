2c34a4877331034002be1c0fb9c9e78c9f55e630
BetaDv <62099784+BetaDv@users.noreply.github.com>
2021-09-02 03:34:07 +0300

    Update afk.js

--- src/commands/afk.js
+++ src/commands/afk.js
@@ -1,34 +1,18 @@
- let S = require('@discordjs/builders').SlashCommandBuilder;
-let s = [{
+module.exports = {
   name: "afk",
   permissions: [],
-  type: "command",
   description: "Set your Status to AFK.",
-  async execute(message, args, client) {
-    if(await client.db.get("afk_" + message.author.id) !== null) return;
+  execute(message, args, client) {
+    if(client.db.get("afk_" + message.author.id) !== null) return;
     let reason = args.join(" ") || null;
     let oldName = message.member.nickname || message.author.username;
-    
-    await client.db.set(`afk_${message.author.id}`, {
+
+    client.db.set(`afk_${message.author.id}`, {
       reason,
       name: oldName
     })
 
     message.member.setNickname("[AFK] " + oldName);
-    return message.reply(`${message.author.username}, your Status was Set to "AFK". ${(reason === null) ? "" : "\nYour Reasoning is: " + reason}`);
+    return message.channel.send(`Your Status was Set to "AFK". ${(reason === null) ? "" : "Your Reasoning is: " + reason}`);
   }
-}]
-s.push({
-name: 'afk',
-type: 'slash',
-data: new S().setName('afk').setDescription('Go afk or something').addStringOption(s => s.setName('reason').setDescription('Why you wana go afk')),
-execute(interaction,cmd,args,client) {
-const { message, member } = interaction;
-interaction.author = interaction.member.user
-s[0].execute(interaction, 
-[interaction.options.get('reason')], client);
-
-// interaction.reply("test");
-});
-
-module.exports = s;
+};
