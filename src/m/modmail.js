44f769b432f482916782cdaafb30a5458a945dd5
neon <saahilattud@gmail.com>
2021-09-08 12:26:49 -0400

    Update modmail.js

--- src/m/modmail.js
+++ src/m/modmail.js
@@ -62,7 +62,7 @@ async function fetchGuild(message,client,args)  {
   const row = new MessageActionRow().setComponents(client.guilds.cache.filter(async g => {
   return await client.db.get('modmail_'+g.id) &&  g.members.cache.get(message.author.id)
   }).map((g,i) => {
-    return new MessageButton().setCustomId(g.id).setLabel(`${i}`).setStyle('PRIMARY')
+    return new MessageButton().setCustomId(g.id).setLabel(`--${i}-`).setStyle('PRIMARY')
   }).slice(0,5))
   let embed = new MessageEmbed().setAuthor(client.user.tag,client.user.displayAvatarURL()).setTitle('Choose a guild').setDescription(client.guilds.cache.filter(async g => g.members.cache.get(message.author.id) && await client.db.get('modmail_'+g.id)).map((g,i) => {
     return ` (${i+1}) - [${g.name}](https://discord.com/channels/${g.id})`
@@ -79,6 +79,7 @@ collecter.on('collect', i => {
   const cmd = i.customId
   if(cmd === 'back_modmail') {}
   if(cmd === 'next_modmail') {}
+  i.deferReply()
   i.message.edit({ 
     components: i.message.components.map(c => {
      return c.options.map(b => {
