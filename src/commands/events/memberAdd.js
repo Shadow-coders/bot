0c883fe7d49bd62e77f6f05c971005b92d06076a
neon <saahilattud@gmail.com>
2021-09-10 14:29:42 -0400

    Update memberAdd.js

--- src/commands/events/memberAdd.js
+++ src/commands/events/memberAdd.js
@@ -6,23 +6,38 @@ module.exports = {
   async execute(member, client) {
     // console.log(member)
     const { guild } = member;
-    const ch = client.db.get("welcome_" + guild.id);
+    const csh = client.db.get("welcome_" + guild.id);
     if (member.user.id === client.user.id) return;
-    if (!ch) return;
-    if (!invites[member.guild.id])
+    if (!csh) return;
+    const {ch,msg} = csh;
+    if (!invites[member.guild.id] && member.guild.me.permissions.has('MANAGE_SERVER'))
       invites[member.guild.id] = await member.guild.invites.fetch();
+      const gInvites = await member.guild.invites.fetch();
+      const invite = gInvites.find(
+        (inv) => invites.get(inv.code).uses < inv.uses
+      );
+      
+    let { inviter } = invite;
+    let fullmsg = client.util.massreplace(msg, [{ word: /{user.name}/, replaced: member.user.name}, {word: /{user.id}/, replaced: member.user.id }, { word: /{user.tag}/, replaced: member.user.tag }, {
+      word: /{test}/,
+      replaced: 'true'
+}, {
+  word: /{inviter.name}/,
+  replaced: inviter.username
+}, {
+  word: /{invite.type}/,
+  replaced: invitetype
+}])
     if (member.user.bot)
       return client.channels.cache
         .get(ch)
-        .send(`**${member.user.tag}**, has joined using Oauth2`);
-    const gInvites = await member.guild.invites.fetch();
-    const invite = gInvites.find(
-      (inv) => invites.get(inv.code).uses < inv.uses
-    );
-    let { inviter } = invite;
+        .send(fullmsg);
+    
+    
+    
     client.channels.cache
       .get(ch)
-      .send(`**${member.user.tag}** has been invited by ${inviter}!`);
+      .send(fullmsg);
     // member.user.send('Welcome to ' + guild.name)
   },
 };
