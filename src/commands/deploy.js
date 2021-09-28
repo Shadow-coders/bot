b910f1613be9b8a841f1121649aa9a2f8ed7693e
neon <saahilattud@gmail.com>
2021-09-28 17:18:19 -0400

    e

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -8,8 +8,11 @@ const command = args[0]
 const isGlobal = args[1] ? args[1] === 'global' : false
 if(!client.slash_commands.find(c => c.name == command)) return message.reply('Command ' + command + ' does not exist');
 let cmd = client.slash_commands.find(c => c.name == command);
+client.error(cmd)
 let { name, description, options } = cmd
-if(!isGlobal) message.guild.commands.create(cmd.data ? cmd.data.toJSON() : { name, description, options }).then(() => message.reply(`Deployed '${command.name}' took ${Date.now() - date}ms to this guild`))
-if(isGlobal) client.application.commands.create(cmd.data ? cmd.data.toJSON() : { name, description, options })    .then(() => message.reply(`Deployed '${command.name}' took ${Date.now() - date}ms to global`))
+let cmddata = cmd.data ? cmd.data.toJSON() : { name, description, options }
+client.error(cmddata)
+if(!isGlobal) message.guild.commands.create(cmddata).then(() => message.reply(`Deployed '${command}' took ${Date.now() - date}ms to this guild`))
+if(isGlobal) client.application.commands.create(cmddata).then(() => message.reply(`Deployed '${command}' took ${Date.now() - date}ms to global`))
 }
 }
