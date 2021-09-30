c22471a31b3819850304c9cadc8fd78dfdd1dbb2
neon <saahilattud@gmail.com>
2021-09-30 13:42:19 -0400

    e

--- src/commands/http.js
+++ src/commands/http.js
@@ -2,9 +2,11 @@ module.exports = {
   name: "http",
   execute(message, args, client) {
     const codes = require("http").STATUS_CODES
-    if (codes[args[0]]) {
-      message.channel.send(`**${args[0]}**, 
-      \n **${codes[args[0]]}**`​);
+    const numb = args[0]
+    if (codes[numb]) {
+
+      message.channel.send((`**${numb}**, 
+      \n **${codes[numb]}**`​));
     } else {
       message.channel.send(" Invalid http status code!");
     }
