ddfee4e54fa59640f246fb99949d3664243f8855
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-10-16 19:58:52 -0500

    Update eval.ts

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -20,6 +20,8 @@ export default [
         let response;
         if (typeof text === "string")
           return text
+.replace(/“/,"\"")
+.replace(/”/, "\"")
             .replace(/`/g, "`" + String.fromCharCode(8203))
             .replace(/@/g, "@" + String.fromCharCode(8203));
         else return text;
