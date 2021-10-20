6bf8247ac4e21e5494c8a7f4349acba224767ac1
neon <saahilattud@gmail.com>
2021-10-20 12:50:21 -0400

    e

--- src/commands/animals.ts
+++ src/commands/animals.ts
@@ -17,5 +17,23 @@ message.reply({
 });        
 
 }
+    },
+    {
+        name: 'dog',
+        execute: async (message: Message, args: String[], client: Shadow) => {
+            //@ts-ignore
+const url = await client.fetch('https://api.shitapi.ga/animals/dog').then(res => res.json()).then((d) => d.dog)
+message.reply({
+    embeds: [
+        new MessageEmbed()
+        .setTitle('dog image')
+        .setColor('RANDOM')
+        .setImage(url)
+        .setTimestamp()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+    ]
+});        
+
+}
     }
 ]
