20b32433c58f158725d95bbd2deb1e645cacf452
neon <saahilattud@gmail.com>
2021-10-20 20:49:00 -0400

    eee

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -34,7 +34,7 @@ export default [
       const isGlobal = args[1] ? args[1] === "--global" : false;
       const All = args[2] ? args[2] === "--all" : false
       console.log(commands.some((c: any) => c?.name === command))
-      if (!client.commands.find((c: any) => c && c.name == command))
+      if (!commands.find((c: any) => c && c.name == command))
         return message.reply("Command " + command + " does not exist\n did you mean `" + commands[0].name + "`?");
       
         let cmd = commands.find((c: any) => c?.name == command);
