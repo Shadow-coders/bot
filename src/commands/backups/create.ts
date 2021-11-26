5e45a042fdfec9987fb9a0bb13f6b07eaed7ef1e
neon <saahilattud@gmail.com>
2021-11-25 19:30:49 -0500

    eee

--- src/commands/backups/create.ts
+++ src/commands/backups/create.ts
@@ -1,6 +1,8 @@
-import { Message, CommandInteraction, Shadow, Guild } from "../../client" 
+import { Message, CommandInteraction, Shadow, Guild, MessageAttachment } from "../../client" 
 import { create, fetch } from 'discord-backup'
 import { Command } from '../../util/commands'
+import { readFileSync } from 'fs'
+let Video = new MessageAttachment(readFileSync(process.cwd() + '/src/util/Role.mp4'), 'role.mp4')
 const ops = {
 name: 'backup-create',
 description: 'description',
@@ -20,8 +22,16 @@ async execute(message:Message,args:String[],client:Shadow) {
     if(message.guild?.id.startsWith('5')) ID += 'M'
     if(message.guild?.id.includes('5')) ID += 'E'
     ID += (Math.random() * 1000).toString().slice(0,3).replace('.', '0')
+const error = (type:string) => {
+    if(type == 'user') return message.reply(`Your missing Perms \`MANAGE_SERVER\``)
+    if(type == 'me') return message.reply(`I am missing \`ADMINISTRATOR\` perms!\n i Need these perms to grab the roles, channels, bans, etc `)
+    if(type == 'role') return message.reply({ content: `I am not the highest role in this server! Please make me have the highest role to grab the roles`, files: [Video] })
+return message.reply(`UNKOWEN_ERROR\n\`${require('util')(type)}\``)
+} 
 
-    if(!message.member?.permissions.has('MANAGE_GUILD')) return 
+if(!message.member?.permissions.has('MANAGE_GUILD')) return error('user');
+    if(!message.guild?.me?.permissions.has("ADMINISTRATOR")) return error('me');
+if((message.guild.me.roles.highest.position !== message.guild.roles.highest.position)) return error('role');
 
 
    await create((message.guild as Guild), {
@@ -37,15 +47,16 @@ doNotBackup: []
         client.error ? client.error(err) :null
         message.reply(err.message || "ERROR!! " + err)
     })
+
 }
 },
 {
 name: ops.name, 
-descripton: ops.description,
+description: ops.description,
 aliases: ops.aliases,
 type: 'slash', 
 async execute(interaction:CommandInteraction,cmd:string,args:any[],client:Shadow) {
     
 }
 }
-]
+] as Array<Command>
